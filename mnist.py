a7b6e28b5cb9376bf753c1157fd0119655f591ec
Thai Trinh <trinhquangthai@gmail.com>
2018-10-20 11:28:47 +0200

    start a simple mnist example

--- mnist.py
+++ mnist.py
@@ -1,6 +1,14 @@
 import keras
 from keras.datasets import mnist
+from keras.models import Sequential
 
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-print(X_train.shape)
+def main():
+    """
+    Simple NN with keras
+    :return:
+    """
+    # load data
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    print(X_train.shape, y_train.shape)
+    print(X_test.shape, y_test.shape)
